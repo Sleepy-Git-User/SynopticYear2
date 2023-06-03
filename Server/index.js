3d5d598f4958cf9e4806567a190a28c918868196
Sleepy <joshfranks910@gmail.com>
2023-06-03 18:09:10 +0100

    login and user maker functions

--- Server/index.js
+++ Server/index.js
@@ -5,41 +5,41 @@ const express = require("express");
 const cors = require("cors");
 const app = express();
 
-// // CORS rubbish
-// app.use(
-// 	cors({
-// 		origin: "http://127.0.0.1:5173",
-// 		optionsSuccessStatus: 200,
-// 	})
-// );
+// CORS rubbish
+app.use(
+	cors({
+		origin: "http://127.0.0.1:5173",
+		optionsSuccessStatus: 200,
+	})
+);
 
-// app.use(
-// 	express.static(path.resolve(__dirname, "../client/dist"), { index: false })
-// );
+app.use(
+	express.static(path.resolve(__dirname, "../client/dist"), { index: false })
+);
 
-// app.use(express.json()); // for parsing application/json
-// app.use(express.urlencoded({ extended: true }));
+app.use(express.json()); // for parsing application/json
+app.use(express.urlencoded({ extended: true }));
 
-// // Server Middleware
-// app.use((req, res, next) => {
-// 	const start = +new Date();
-// 	next();
-// 	const time = +new Date() - start;
-// 	console.log("Request made to", req.path, "took", `${time}ms`);
-// });
+// Server Middleware
+app.use((req, res, next) => {
+	const start = +new Date();
+	next();
+	const time = +new Date() - start;
+	console.log("Request made to", req.path, "took", `${time}ms`);
+});
 
-// // Server Config
+// Server Config
 const port = process.env.PORT || 3000;
 
-// // Routers
-// const APIRoute = require("./routes/api")({ express });
+// Routers
+const APIRoute = require("./routes/api")({ express });
 
-// // Routes
-// app.use("/api", APIRoute);
+// Routes
+app.use("/api", APIRoute);
 
-// app.get("*", (req, res) => {
-// 	res.sendFile(path.resolve(__dirname, "../Client/dist", "index.html"));
-// });
+app.get("*", (req, res) => {
+	res.sendFile(path.resolve(__dirname, "../Client/dist", "index.html"));
+});
 
 app.set("view engine", "ejs");
 
