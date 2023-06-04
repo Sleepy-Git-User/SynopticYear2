47f12a9332b7d67ec7999cd0a75b528a4efbaec1
Sleepy <joshfranks910@gmail.com>
2023-06-04 12:33:33 +0100

    s

--- Server/routes/api.js
+++ Server/routes/api.js
@@ -36,7 +36,7 @@ module.exports = (components) => {
 
 	router.post("/loginChecker", async (req, res) => {
 		try {
-			const { Email, Password } = req.body;
+			const {Email, Password} = req.body;
 			if (!Email || !Password ) {
 				return res
 					.status(400)
