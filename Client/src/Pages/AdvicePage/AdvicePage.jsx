444177fea7c05c6de3840535416ead06d8a23782
Chanelmorgan <chanelmorgan05@icloud.com>
2023-06-08 15:52:40 +0100

    advice boxes
    Co-Authored-By: Grace Johnson <106590642+gracetoxicstar02@users.noreply.github.com>

--- Client/src/Pages/AdvicePage/AdvicePage.jsx
+++ Client/src/Pages/AdvicePage/AdvicePage.jsx
@@ -14,15 +14,39 @@ export default function AdvicePage() {
             <div class="gridContainerAdvice">
                 <div class="infoBox1">
                     <h2>Budget Meals</h2>
+                    <div className="HealthBox1"> 
+                    <p> £5 Meals </p>
+                    </div>
+                    <div className="HealthBox2"> 
+                    <p> £5 Meals </p>
+                    </div>
                 </div>
                 <div class="infoBox2">
                     <h2>Schemes and Charities</h2>
+                    <div className="CharityBox1"> 
+                    <p> £5 Meals </p>
+                    </div>
+                    <div className="CharityBox2"> 
+                    <p> £5 Meals </p>
+                    </div>
                 </div>
                 <div class="infoBox3">
-                    <h2>Deals</h2>
+                    <h2>Deals</h2> 
+                    <div className="DealsBox1"> 
+                    <p> £5 Meals </p>
+                    </div>
+                    <div className="DealsBox2"> 
+                    <p> £5 Meals </p>
+                    </div>
                 </div>
                 <div class="infoBox4">
                     <h2>Community Events</h2>
+                    <div className="EventsBox1"> 
+                    <p> £5 Meals </p>
+                    </div>
+                    <div className="EventsBox2"> 
+                    <p> £5 Meals </p>
+                    </div>
                 </div>
             </div>
 
