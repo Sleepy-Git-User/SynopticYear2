a7950a5ba5d6b03238b4bfe72589ea98b3e734b9
Sleepy <joshfranks910@gmail.com>
2023-06-10 23:41:07 +0100

    added Token Making and Reading

--- Server/Auth/Auth.js
+++ Server/Auth/Auth.js
@@ -1,4 +1,19 @@
-function makeCookie(){
-    console.log("Heres a cookie!");
+const jwt = require('jsonwebtoken')
+require("dotenv").config();
+
+// Takes UserID and using JWT and a Secret key from .env makes a web token.
+function makeToken(userID){
+    const accessToken = jwt.sign(userID,process.env.ACCESS_TOKEN_SECRET)
+    return accessToken;    
 }
-module.exports={makeCookie};
+
+// Takes a JWT and using the Secret key converts it in to a UserID
+function readToken(givenToken){
+    user = jwt.verify(givenToken,process.env.ACCESS_TOKEN_SECRET)
+    return user;
+}
+
+// Makes then Reads Token.
+//console.log(readToken(makeToken("f987b616-7008-4257-af6e-cb2239a52def")));
+
+module.exports={makeToken,readToken};
