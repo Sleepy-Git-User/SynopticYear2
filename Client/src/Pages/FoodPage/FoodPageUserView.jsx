fa6e58d4fc3bddf6f533967fd3f0fee7b2967747
Chanelmorgan <chanelmorgan05@icloud.com>
2023-06-12 10:32:51 +0100

    Update FoodPageUserView.jsx
    Co-Authored-By: Grace Johnson <106590642+gracetoxicstar02@users.noreply.github.com>

--- Client/src/Pages/FoodPage/FoodPageUserView.jsx
+++ Client/src/Pages/FoodPage/FoodPageUserView.jsx
@@ -10,7 +10,7 @@ export default function FoodPageUserView() {
 
   const [filter, setFilter] = useState({
     Vegan: false,
-    Vegetarian: false,
+    Vegiterian: false,
     Halal: false,
     Kosher: false,
   });
@@ -131,34 +131,6 @@ export default function FoodPageUserView() {
                 </div>
                 <div className='listingsBox'>
                     <h3>Listings</h3>
-                    {/*Call a function to get all listings on a useEffect, and call it again when a filter is changed.*/}
-                    {/*Each one should have a form to specify quantity and a button to submit, on which a function call is done to reserve the item*/}
-                    {listings.map((listing) => (
-                        <form key={listing.ListingID}>
-                            <div>{listing.Name}</div>
-                            <div>{listing.Description}</div>
-                            <div>Price: £{listing.Price}</div>
-                            <div>Total Quantity: {listing.Quantity}</div>
-                            <label htmlFor="name">Quantity: </label>
-                        <input
-                            type="number"
-                            id="quantity"
-                            name="Quantity"
-                            default="1"
-                            value={QuantityValue}
-                            onChange={(e) => setQuantityValue(e.target.value)}
-                            max={listing.Quantity}
-                        />
-                        <button onClick={() => reserveItem({ListingID: listing.ListingID, BuyerID: sessionStorage.getItem("userId"), Quantity: QuantityValue.toString() })}>Reserve</button>
-                        </form>
-                    ))}
-                </div>
-                </div>
-
-
-        </div>
-        <div>
-
           {/*Call a function to get all listings on a useEffect, and call it again when a filter is changed.*/}
           {/*Each one should have a form to specify quantity and a button to submit, on which a function call is done to reserve the item*/}
           {listings.map((listing) => (
@@ -183,7 +155,12 @@ export default function FoodPageUserView() {
 
             </form>
           ))}
+                </div>
+                </div>
+
+
         </div>
+
 
 
       </div>
