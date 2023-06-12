d6b92872bca88202efd978c0f958f8c3a2e650d1
Wilsonator123 <106590637+Wilsonator123@users.noreply.github.com>
2023-06-12 10:30:26 +0100

    Update FoodPageUserView.jsx

--- Client/src/Pages/FoodPage/FoodPageUserView.jsx
+++ Client/src/Pages/FoodPage/FoodPageUserView.jsx
@@ -10,7 +10,7 @@ export default function FoodPageUserView() {
 
   const [filter, setFilter] = useState({
     Vegan: false,
-    Vegetarian: false,
+    Vegitarian: false,
     Halal: false,
     Kosher: false,
   });
@@ -110,89 +110,89 @@ export default function FoodPageUserView() {
       <div class="mainInfo">
         <h2>User</h2>
         <div>
-            
-              
-            <div id="pageContainer">  
-                <div className='userViewGrid'>
-                
-                <div className='filtersBox'>
-                    <h3>Filters</h3>
-                
-                    {categories.map((category) => (
-            <div key={category.CategoryID}>
-              <input
-                type="checkbox"
-                id={category.CategoryID}
-                name="Category"
-                value={category.Name}
-                checked={category[category.Name]}
-                onChange={handleCategoryChange}
-              />
-              <label htmlFor={category.CategoryID}>{category.Name}</label>
+
+
+          <div id="pageContainer">
+            <div className='userViewGrid'>
+
+              <div className='filtersBox'>
+                <h3>Filters</h3>
+
+                {categories.map((category) => (
+                  <div key={category.CategoryID}>
+                    <input
+                      type="checkbox"
+                      id={category.CategoryID}
+                      name="Category"
+                      value={category.Name}
+                      checked={category[category.Name]}
+                      onChange={handleCategoryChange}
+                    />
+                    <label htmlFor={category.CategoryID}>{category.Name}</label>
+                  </div>
+                ))}
+              </div>
+              <div className='listingsBox'>
+                <h3>Listings</h3>
+                {/*Call a function to get all listings on a useEffect, and call it again when a filter is changed.*/}
+                {/*Each one should have a form to specify quantity and a button to submit, on which a function call is done to reserve the item*/}
+                {listings.map((listing) => (
+                  <form key={listing.ListingID}>
+                    <div>{listing.Name}</div>
+                    <div>{listing.Description}</div>
+                    <div>Price: £{listing.Price}</div>
+                    <div>Total Quantity: {listing.Quantity}</div>
+                    <label htmlFor="name">Quantity: </label>
+                    <input
+                      type="number"
+                      id="quantity"
+                      name="Quantity"
+                      default="1"
+                      value={QuantityValue}
+                      onChange={(e) => setQuantityValue(e.target.value)}
+                      max={listing.Quantity}
+                    />
+                    <button onClick={() => reserveItem({ ListingID: listing.ListingID, BuyerID: sessionStorage.getItem("userId"), Quantity: QuantityValue.toString() })}>Reserve</button>
+                  </form>
+                ))}
+              </div>
             </div>
-          ))}
-                </div>
-                <div className='listingsBox'>
-                    <h3>Listings</h3>
-                    {/*Call a function to get all listings on a useEffect, and call it again when a filter is changed.*/}
-                    {/*Each one should have a form to specify quantity and a button to submit, on which a function call is done to reserve the item*/}
-                    {listings.map((listing) => (
-                        <form key={listing.ListingID}>
-                            <div>{listing.Name}</div>
-                            <div>{listing.Description}</div>
-                            <div>Price: £{listing.Price}</div>
-                            <div>Total Quantity: {listing.Quantity}</div>
-                            <label htmlFor="name">Quantity: </label>
-                        <input
-                            type="number"
-                            id="quantity"
-                            name="Quantity"
-                            default="1"
-                            value={QuantityValue}
-                            onChange={(e) => setQuantityValue(e.target.value)}
-                            max={listing.Quantity}
-                        />
-                        <button onClick={() => reserveItem({ListingID: listing.ListingID, BuyerID: sessionStorage.getItem("userId"), Quantity: QuantityValue.toString() })}>Reserve</button>
-                        </form>
-                    ))}
-                </div>
-                </div>
 
 
-        </div>
-        <div>
+          </div>
+          <div>
 
-          {/*Call a function to get all listings on a useEffect, and call it again when a filter is changed.*/}
-          {/*Each one should have a form to specify quantity and a button to submit, on which a function call is done to reserve the item*/}
-          {listings.map((listing) => (
-            <form key={listing.ListingID}>
-              <img src={listing.img} alt="Listing Image" width="75" height="75" />
-              <div>{listing.Name}</div>
-              <div>{listing.Description}</div>
-              <div>Price: £{listing.Price}</div>
-              <div>Total Quantity: {listing.Quantity}</div>
-              <label htmlFor="name">Quantity: </label>
-              <input
-                type="number"
-                id="quantity"
-                name="Quantity"
-                default="1"
-                value={QuantityValue}
-                onChange={(e) => setQuantityValue(e.target.value)}
-                max={listing.Quantity}
-              />
-              <button onClick={() => reserveItem({ ListingID: listing.ListingID, BuyerID: sessionStorage.getItem("userId"), Quantity: QuantityValue.toString() })}>Reserve</button>
+            {/*Call a function to get all listings on a useEffect, and call it again when a filter is changed.*/}
+            {/*Each one should have a form to specify quantity and a button to submit, on which a function call is done to reserve the item*/}
+            {listings.map((listing) => (
+              <form key={listing.ListingID}>
+                <img src={listing.img} alt="Listing Image" width="75" height="75" />
+                <div>{listing.Name}</div>
+                <div>{listing.Description}</div>
+                <div>Price: £{listing.Price}</div>
+                <div>Total Quantity: {listing.Quantity}</div>
+                <label htmlFor="name">Quantity: </label>
+                <input
+                  type="number"
+                  id="quantity"
+                  name="Quantity"
+                  default="1"
+                  value={QuantityValue}
+                  onChange={(e) => setQuantityValue(e.target.value)}
+                  max={listing.Quantity}
+                />
+                <button onClick={() => reserveItem({ ListingID: listing.ListingID, BuyerID: sessionStorage.getItem("userId"), Quantity: QuantityValue.toString() })}>Reserve</button>
 
 
-            </form>
-          ))}
+              </form>
+            ))}
+          </div>
+
+
         </div>
 
 
       </div>
-
-
-    </div>
     </div>
   );
 }
