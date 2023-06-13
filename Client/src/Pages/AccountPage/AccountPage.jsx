ec8144471844d976a70c1d9c1f99202880f75809
Chanelmorgan <chanelmorgan05@icloud.com>
2023-06-13 11:50:15 +0100

    changing image
    Co-Authored-By: Grace Johnson <106590642+gracetoxicstar02@users.noreply.github.com>

--- Client/src/Pages/AccountPage/AccountPage.jsx
+++ Client/src/Pages/AccountPage/AccountPage.jsx
@@ -36,7 +36,7 @@ export default function AccountPage() {
                 <div className="userInfo"> 
                 <h2>User Details</h2>
                 <div className="info">
-                <img src={info.data[0].img} alt="Profile Image" width="75" height="75" />
+                <img className="userImg" src={info.data[0].img} alt="Profile Image" width="75" height="75" />
                   <br />
                   Name: {info.data[0].Fname} {info.data[0].Lname} <br />
                   Email: {info.data[0].Email}<br />
